7493b8bc271e776ffef25bb205811c4687174050
Aravin <aravin.it@gmail.com>
2021-08-15 01:08:11 +0530

    Completed Home Page

--- frontend/components/card.js
+++ frontend/components/card.js
@@ -1,15 +1,18 @@
 import React from "react"
 import Link from "next/link"
 import NextImage from "./image"
+import moment from "moment"
 
 const Card = ({ article }) => {
   return (
     <Link as={`/article/${article.slug}`} href="/article/[id]">
       <a>
-        <div className="card shadow-lg bg-white ">
+        <div className="card bg-white shadow-2xl mb-5">
           <div className="card-body">
             <h2 className="card-title">{article.title}</h2>
-            <p>Category: {article.category.name}</p>
+            <p><span className="font-semibold">Written </span>{moment(article.created_at).fromNow()}</p>
+            <p><span className="font-bold">Category</span> {article.category.name}</p>
+            {/* <p><span className="font-semibold">Tags</span> {article.tags.reduce((_m __))}</p> */}
           </div>
         </div>
       </a>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -13,6 +13,9 @@ import {
   faTwitter,
   faFacebook,
 } from "@fortawesome/free-brands-svg-icons"
+import emiCalculatorImage from "../public/projects/emi-calculator.png"
+import sipCalculatorImage from "../public/projects/sip-calculator.png"
+import warrantyManagerImage from "../public/projects/warranty-manager.jpg"
 
 const Home = ({ articles, categories, homepage }: any) => {
   return (
@@ -118,124 +121,228 @@ const Home = ({ articles, categories, homepage }: any) => {
           </div>
         </div>
 
-        <div className="hero bg-white rounded px-5 py-10 mb-5">
-          <h2 className="text-3xl font-bold">Projects</h2>
+        <div className="bg-white rounded px-5 py-5 mb-5 w-full">
+          <div className="text-center">
+            <h2 className="mb-5 text-3xl font-bold">Projects</h2>
+          </div>
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center place-self-stretch">
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    width="400"
+                    height="250"
+                    layout="responsive"
+                    src="/projects/servicebird.png"
+                    className="rounded-xl"
+                    alt="servicebird.in"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://www.servicebird.in"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      ServiceBird.in
+                    </a>
+                  </h2>
+                  <p>Local Home Appliance Service Company in Chennai</p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Next.js</div>
+                      <div className="badge badge-primary m-2">React.js</div>
+                      <div className="badge badge-primary m-2">JavaScript</div>
+                      <div className="badge badge-primary m-2">TypeScript</div>
+                      <div className="badge badge-primary m-2">Vercel</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    width="400"
+                    height="250"
+                    layout="responsive"
+                    src="/projects/epix.io.png"
+                    className="rounded-xl"
+                    alt="epix.io Project"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://www.epix.io"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      epix.io
+                    </a>
+                  </h2>
+                  <p>
+                    Upcoming open source project with everyday tools and
+                    services
+                  </p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Angular.js</div>
+                      <div className="badge badge-primary m-2">TypeScript</div>
+                      <div className="badge badge-primary m-2">Docker</div>
+                      <div className="badge badge-primary m-2">Firebase</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    width="400"
+                    height="250"
+                    layout="responsive"
+                    src="/projects/frontline-courier.png"
+                    className="rounded-xl"
+                    alt="epix.io Project"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://www.frontlinecourier.com"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      FrontlineCourier.com
+                    </a>
+                  </h2>
+                  <p>
+                    Courier company website with client and admin portal with
+                    end-to-end solution from booking to tracking the courier.
+                  </p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Angular.js</div>
+                      <div className="badge badge-primary m-2">TypeScript</div>
+                      <div className="badge badge-primary m-2">Firebase</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src={sipCalculatorImage}
+                    layout="responsive"
+                    className="rounded-xl"
+                    alt="epix.io Project"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://play.google.com/store/apps/details?id=io.epix.sip"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      SIP Calculator
+                    </a>
+                  </h2>
+                  <p>
+                    SIP, SWP, STP and Lumpsum returns Calculator Android App
+                  </p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Flutter</div>
+                      <div className="badge badge-primary m-2">Android</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src={warrantyManagerImage}
+                    layout="responsive"
+                    className="rounded-xl"
+                    alt="epix.io Project"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://play.google.com/store/apps/details?id=io.epix.warranty_manager"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Warranty Manager
+                    </a>
+                  </h2>
+                  <p>
+                    Android application to manage your product or service
+                    warranty
+                  </p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Flutter</div>
+                      <div className="badge badge-primary m-2">Android</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-5 rounded">
+              <div className="card text-center shadow-2xl">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src={emiCalculatorImage}
+                    layout="responsive"
+                    className="rounded-xl"
+                    alt="epix.io Project"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    <a
+                      href="https://play.google.com/store/apps/details?id=io.epix.emi"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      EMI Calculator
+                    </a>
+                  </h2>
+                  <p>
+                    Android application to calculate the Loan EMI and Compare Loans
+                  </p>
+                  <div className="justify-center card-actions">
+                    <div>
+                      <div className="badge badge-primary m-2">Flutter</div>
+                      <div className="badge badge-primary m-2">Android</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div>
-          {/* <h1>{homepage.hero.title}</h1> */}
-          <Articles articles={articles} />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+        <div className="bg-white rounded px-5 py-5 mb-5 w-full">
+          <div className="text-center">
+            <h2 className="mb-5 text-3xl font-bold">Recent Writings</h2>
+          </div>
+          <div>
+            <Articles articles={articles} />
+          </div>
         </div>
+
       </div>
     </Layout>
   )
