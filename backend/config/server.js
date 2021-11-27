21c71be6777ace7218a764f5ceb20655365efeaf
Aravin <aravin.it@gmail.com>
2021-11-27 10:45:25 +0530

    Updated server.js - backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: 1337,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: process.env.ADMIN_JWT_SECRET,
+      secret: env('ADMIN_JWT_SECRET', ''),
     },
   },
 });
