77f003eab2e2c5b33a1cd002308b47e343f5fbc1
Aravin <aravin.it@gmail.com>
2022-07-23 21:19:50 +0530

    updated slug

--- frontend/pages/article/[slug].tsx
+++ frontend/pages/article/[slug].tsx
@@ -84,10 +84,9 @@ const Article = ({ article, categories }: any) => {
 
 export async function getStaticPaths() {
   const articles = await fetchAPI("/articles")
-  console.log(articles)
 
   return {
-    paths: articles.data?.map((article: { attributes: any }) => ({
+    paths: articles.map((article: any) => ({
       params: {
         slug: article.attributes.slug,
       },
