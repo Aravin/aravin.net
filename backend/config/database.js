d90dbfb0f4d6750901884eb562c2f91cd5fe91ec
Aravin <4869265+Aravin@users.noreply.github.com>
2022-10-27 22:32:53 +0530

    Updated strapi to 4.4.5

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('INSTANCE_CONNECTION_NAME', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'aravin_net_local'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+      user: env('DATABASE_USER', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
