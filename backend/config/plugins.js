4a9c3a0a725090aae0b5b43b122786f4d4577b9f
Aravin <4869265+Aravin@users.noreply.github.com>
2023-07-25 22:14:31 +0530

    updated strapi s3 config

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,24 +1,26 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
+  // ...
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
           accessKeyId: env('AWS_ACCESS_KEY_ID'),
           secretAccessKey: env('AWS_ACCESS_SECRET'),
           region: env('AWS_REGION'),
           params: {
             Bucket: env('AWS_BUCKET'),
           },
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+        }
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    seo: {
-      enabled: true,
-    }
-  });
+  },
+  seo: {
+    enabled: true,
+  }
+});
